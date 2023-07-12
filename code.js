41e26491fd613be791de7492d3f835f402b33841
Anas Hasna <hasnaanas230@gmail.com>
2023-07-12 13:59:56 +0100

    reminiscing old js coding commit

--- code.js
+++ code.js
@@ -1,32 +1,32 @@
-const decrease=document.getElementById('decrease');
-const increase=document.getElementById('increase');
-const reseat=document.getElementById('reseat');
-let elt=document.getElementById('count');
-let anas=0;
+const decrease = document.getElementById("decrease");
+const increase = document.getElementById("increase");
+const reset = document.getElementById("reset");
+let elt = document.getElementById("count");
+let count = 0;
 update();
 
-increase.addEventListener("click",()=>{
-    anas++;
-    update();
-}) ;
+increase.addEventListener("click", () => {
+  count++;
+  update();
+});
 
-decrease.addEventListener("click",()=>{
-    anas--;
-    update();
-}) ;
+decrease.addEventListener("click", () => {
+  count--;
+  update();
+});
 
-reseat.addEventListener("click",()=>{
-    anas=0;
-    update();
-}) ;
+reset.addEventListener("click", () => {
+  count = 0;
+  update();
+});
 
-
-function update(){
-    elt.innerHTML=anas;
-    if (anas<'0'){
-        elt.style.color="red";
-    }
-    else if (anas>='0'){
-        elt.style.color="black";
-    }
+function update() {
+  elt.innerHTML = count;
+  if (count < 0) {
+    elt.style.color = "red";
+  } else if (count == 0) {
+    elt.style.color = "black";
+  } else {
+    elt.style.color = "green";
+  }
 }
